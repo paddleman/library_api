a976cac2378a3cdf26627f1942ed3b278f120af4
DJ <rivers.creeks@gmail.com>
2019-03-09 12:05:42 -0800

    completed video 7 - added author domain

---
+++ priv/repo/migrations/20190309194118_create_authors.exs
@@ -0,0 +1,13 @@
+defmodule LibraryApi.Repo.Migrations.CreateAuthors do
+  use Ecto.Migration
+
+  def change do
+    create table(:authors) do
+      add :first, :string
+      add :last, :string
+
+      timestamps()
+    end
+
+  end
+end
